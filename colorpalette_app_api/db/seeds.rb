71a59670d7464fd40929a59993de48a149cb8fdd
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-06 15:30:58 -0500

    seeding data to app

--- colorpalette_app_api/db/seeds.rb
+++ colorpalette_app_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Palette.create([
+    { color_name: 'blue', img: 'https://s-media-cache-ak0.pinimg.com/736x/9d/11/3c/9d113cba30477439caaf3acffb3f96e8.jpg' },
+    { color_name: 'yellow', img: 'https://s-media-cache-ak0.pinimg.com/736x/7b/cf/0e/7bcf0e84b60e6d1e9a12a938503cfb2d.jpg' },
+    { color_name: 'red', img: 'https://s-media-cache-ak0.pinimg.com/736x/31/43/1c/31431ce5575503cbb457d3114eac1726.jpg' },
+    { color_name: 'green', img: 'http://static.tumblr.com/fanrnac/Qz8mb14j4/pantone_color_easter_green.jpg' },
+    { color_name: 'purple', img: 'https://s-media-cache-ak0.pinimg.com/564x/36/ac/90/36ac90cf2269118905cb5467f42abdcd.jpg' },
+    { color_name: 'orange', img: 'https://s-media-cache-ak0.pinimg.com/736x/ab/d1/82/abd1825cadbf6814442bb97bd1eeffbe.jpg' }
+])
